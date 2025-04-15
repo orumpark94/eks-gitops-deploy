8c7d7b4bf9c0be69cab5958abfe1d4729dae1ac4
orumpark94 <nowd940@gmail.com>
2025-04-15 09:19:14 +0900

    first update 2

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "eks-gitops-tfstate"
+    bucket         = "eks-gitops-tfstate-20250415
     key            = "eks/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
