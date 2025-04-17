184ff6dab2e262149173ecf012735d47ae9d6519
orumpark94 <nowd940@gmail.com>
2025-04-17 16:04:19 +0900

    public ip launch

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,6 +18,7 @@ resource "aws_subnet" "public_subnet_a" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-2a"
+  map_public_ip_on_launch = true # ✅ 수정됨
 
   tags = {
     Name = "eks-public-a"
@@ -29,7 +30,8 @@ resource "aws_subnet" "public_subnet_c" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-northeast-2c"
-
+  map_public_ip_on_launch = true # ✅ 수정됨
+  
   tags = {
     Name = "eks-public-c"
   }
