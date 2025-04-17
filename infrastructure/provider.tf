d6a7ecb6be982b23c3969d1360d37dfc0e5d5cc9
orumpark94 <nowd940@gmail.com>
2025-04-17 15:10:30 +0900

    nodegroup생성에 대한 설정

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -3,9 +3,6 @@ data "aws_eks_cluster" "eks" {
   name = "eks-gitops-cluster"
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.aws_eks_cluster.eks.name
-}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
