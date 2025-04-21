ebf12aed2a49228dcd4880189842550c888a11ff
orumpark94 <nowd940@gmail.com>
2025-04-21 09:30:41 +0900

    내용추가

--- infrastructure/aws-auth.tf
+++ infrastructure/aws-auth.tf
@@ -1,4 +1,11 @@
+variable "enable_aws_auth" {
+  type    = bool
+  default = false
+}
+
 resource "kubernetes_config_map" "aws_auth" {
+  count = var.enable_aws_auth ? 1 : 0
+
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
@@ -9,10 +16,7 @@ resource "kubernetes_config_map" "aws_auth" {
       {
         rolearn  = data.aws_iam_role.worker_node_role.arn
         username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = [
-          "system:bootstrappers",
-          "system:nodes"
-        ]
+        groups   = ["system:bootstrappers", "system:nodes"]
       }
     ])
   }
--- infrastructure/terraform.tfvars
+++ infrastructure/terraform.tfvars
@@ -1,3 +1,4 @@
 aws_region        = "ap-northeast-2"
 cluster_name      = "eks-gitops-cluster"
 cluster_role_name = "eksClusterRole"
+enable_aws_auth   = false
